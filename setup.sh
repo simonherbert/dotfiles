afc908596e3d412bb6db4ff0465353aa65d1badf
Regenwolf <simon.herbert@outlook.de>
2020-05-18 21:02:03 +0200

    add nerdtree install to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
 # Install Ctrl-P Vim Plugin
 git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+vim -u NONE -c ":helptags ~/.vim/bundle/ctrlp.vim/doc" -c q
+
+# Install NERDTree Vim Plugin
+git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
+vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
+
