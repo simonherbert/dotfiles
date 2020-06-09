f5b531962830cfa32ac88eb75f542e9dc181603a
Regenwolf <simon.herbert@outlook.de>
2020-06-09 20:10:30 +0200

    fix fish functions.fish

--- .config/fish/functions.fish
+++ .config/fish/functions.fish
@@ -1,4 +1,4 @@
-function lgcommit# This is the function name and command we call
+function lgcommit # This is the function name and command we call
     git --git-dir=$PWD/.git add . # Stage all unstaged files
     git --git-dir=$PWD/.git commit -a -m $argv # Commit files with the given argument as the commit message
     git --git-dir=$PWD/.git push # Push to remote
