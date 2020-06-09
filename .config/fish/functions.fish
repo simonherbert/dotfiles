0b9d175db708398f1535d2a10bfb5ad3febd7bf0
Regenwolf <simon.herbert@outlook.de>
2020-06-09 16:24:13 +0200

    more lazygit functions

--- .config/fish/functions.fish
+++ .config/fish/functions.fish
@@ -1,5 +1,14 @@
-function lazygit # This is the function name and command we call
+function lgcommit# This is the function name and command we call
     git --git-dir=$PWD/.git add . # Stage all unstaged files
     git --git-dir=$PWD/.git commit -a -m $argv # Commit files with the given argument as the commit message
     git --git-dir=$PWD/.git push # Push to remote
+end
+
+function lgreset
+    git --git-dir=$PWD/.git checkout .
+end
+
+function lgcheckout
+    git --git-dir=$PWD/.git fetch --all
+    git --git-dir=$PWD/.git checkout $argv
 end
